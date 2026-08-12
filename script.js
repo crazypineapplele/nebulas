20626da8e4c8777acdc2a1e204a6445bbabe06d4
Iliyas <45680063+crazypineapplele@users.noreply.github.com>
2026-08-12 17:20:01 +0500

    Replace some nebulae images, add js for button, finish html and start css

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', () => {
+  window.open('https://www.nasa.gov/universe/nebulae/', '_blank', 'noopener,noreferrer');
+});
