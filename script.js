94da3fc091487c4b87f02c4214a21a73d90a633b
Iliyas <45680063+crazypineapplele@users.noreply.github.com>
2026-08-13 12:28:47 +0500

    Remove instructions.txt and reference image, finish whole page

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const button = document.getElementById('myButton');
 button.addEventListener('click', () => {
   window.open('https://www.nasa.gov/universe/nebulae/', '_blank', 'noopener,noreferrer');
 });
+
+button.addEventListener('mouseenter', () => {
+  button.style.backgroundColor = '#F9FAF8'
+  button.style.color = '#3882F6'
+}
+);
+
+button.addEventListener('mouseleave', () => {
+  button.style.backgroundColor = '#3882F6'
+  button.style.color = '#F9FAF8'
+}
+);
